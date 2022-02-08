bfe6e606165b96cc9116457e0f84f4b5f1371c31
alxtpl <alxtpl@gmail.com>
2022-02-08 11:28:53 +0200

    debug

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
-import style from "./ContactForm.module.css";
 import React, { Component } from "react";
+import style from "./ContactForm.module.css";
 
 class ContactForm extends Component {
     state = {
